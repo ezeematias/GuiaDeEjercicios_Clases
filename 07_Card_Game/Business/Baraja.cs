396ea5e5ec6f338cd13d7ce4a8a58267a4929632
ezeematias <eunia69@gmai.com>
2021-04-06 09:28:47 -0300

    EjercicioBarajas

---
+++ 07_Card_Game/Business/Baraja.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Baraja
+    {
+        Carta[] cartas = new Carta[48];
+        public Baraja()
+        {
+            int i = 0;
+            foreach (Carta.Palo item in Carta.Palo.GetValues(typeof(Carta.Palo)))
+            {
+                foreach (Carta.Valor valorActual in Carta.Palo.GetValues(typeof(Carta.Valor)))
+                {
+                    cartas[i] = new Carta(valorActual, item);
+                    i++;
+                    Console.WriteLine("Palo: " + item + "Valor: " + valorActual);
+                }
+            }
+        }
+    }
+}
